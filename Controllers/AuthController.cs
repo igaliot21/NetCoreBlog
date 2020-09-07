cf972235b07656f57016e46fb2ae9525d6b6e669
Jose Mª Izquierdo Galiot <igaliot21@gmail.com>
2020-09-07 15:40:26 +0200

    LogIn & LogOut

---
+++ Controllers/AuthController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using NetCoreBlog.Models;
+using NetCoreBlog.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetCoreBlog.Controllers
+{
+    public class AuthController : Controller
+    {
+        private SignInManager<AppUser> signInManager;
+        public AuthController(SignInManager<AppUser> SignInManager)
+        {
+            this.signInManager = SignInManager;
+        }
+        [HttpGet]
+        public IActionResult Login() {
+            return View(new LoginViewModel());
+        }
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginViewModel vm)
+        {
+            var result = await this.signInManager.PasswordSignInAsync(vm.UserName, vm.Password, false, false);
+            return RedirectToAction("Index","Home");
+        }
+        [HttpGet]
+        public async Task<IActionResult> Logout()
+        {
+            await this.signInManager.SignOutAsync();
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
